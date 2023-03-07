6b9303f686e04b28dc52bc52c60e3cd8df1a60ef
slammysammy2 <113303157+slammysammy2@users.noreply.github.com>
2023-03-07 18:51:37 +0100

    created Footer component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Weather from "./Weather";
+import Footer from "./Footer";
 import "./Weather.css";
 import "./App.css";
 
@@ -11,6 +12,7 @@ root.render(
   <StrictMode>
     <div className="App">
       <Weather />
+      <Footer />
     </div>
   </StrictMode>
 );
