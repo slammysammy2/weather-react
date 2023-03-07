59d204ddee5e1b77ee42d2d7a617c1d61bd2ad34
slammysammy2 <113303157+slammysammy2@users.noreply.github.com>
2023-03-07 18:42:10 +0100

    added footer

--- src/Weather.js
+++ src/Weather.js
@@ -65,7 +65,24 @@ export default function Weather() {
         </div>
       </div>
       <div className="weather-forecast"></div>
-      <div />
+      <footer className="text-center mt-3">
+        This project was coded by Samantha Baran and is open-sourced on{" "}
+        <a
+          href="https://github.com/slammysammy2/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub{" "}
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://remarkable-douhua-f868d5.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify{" "}
+        </a>
+      </footer>
     </div>
   );
 }
